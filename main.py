4d5e00749021a1517491ea74a4879d82ddd00293
Leo Paggen <lpaggen@gmail.com>
2023-11-28 15:16:44 +0100

    several bug fixes

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ class Deck(object):
     rank_names = [None, None, 'Two', 'Three', 'Four', 'Five',
                  'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack',
                  'Queen', 'King', 'Ace']
-    index_tracker = {"i" : 0, "j" : 0} # tracking the i, j indices used later in round()
+    index_tracker = {"i" : 0, "j" : 0} # tracks the i, j indices used later in round()
+    is_war = False # boolean indicator, handles the issue with autoplay when recursion is used
     def __init__(self):
         self.deck = []
         self.p1_deck = []
@@ -77,6 +78,8 @@ class Game(Deck):
     """
     def __init__(self):
         super().__init__() # init attributes of parent class
+        self.autoplay = False
+        self.startround = "y"
 
     def players(self):
         """
@@ -90,65 +93,92 @@ class Game(Deck):
         """
         Starts a new round for both players
         """
-        while len(self.p1_deck) or len(self.p2_deck) > 0:
+        if self.roundcount == 1:
             try:
-                startround = str(input(f"Start round {self.roundcount} ? (y/n)"))
-            except(ValueError):
-                print("Please only input either y or n")
-                self.round()
-            if startround == "y":
+                autoplay = str(input("Autoplay on? (y/n) "))
+                if autoplay == "y":
+                    self.autoplay = True
+                else:
+                    self.autoplay = False
+            except ValueError:
+                print("Invalid input, enter either y or n")
+        while len(self.p1_deck) > 0 or len(self.p2_deck) > 0:
+            if self.autoplay is True:
                 proceed = True
-            elif startround == "n":
-                print("Exiting game..")
-                break
+                self.startround = "y"
+            if self.autoplay is False:
+                try:
+                    self.startround = str(input(f"Start round {self.roundcount} ? (y/n)"))
+                    if self.startround == "y":
+                        proceed = True
+                    elif self.startround == "n":
+                        proceed = False
+                        print("Exiting game..")
+                        exit()
+                except ValueError:
+                    print("Please only input either y or n")
+                    self.round()
 
             if proceed is True:
-                print(f"Player 1 card: {self.p1_deck[0]}\nPlayer 2 card: {self.p2_deck[0]}")
-
                 # manipulate the index tracker dictionary
-                i, j = self.index_tracker.values() # just taking the values and assigning to i, j
+                i, j = self.index_tracker.values() # just takes the values and assigning to i, j
+                print(f"Player 1 card: {self.p1_deck[i]}\nPlayer 2 card: {self.p2_deck[j]}")
 
                 # 3 conditions to check, <, >, ==
-                if self.p1_deck[i] > self.p2_deck[j]: # using i and j, modular
+                if (self.p1_deck[i] > self.p2_deck[j]) and (len(self.p1_deck) > 0 and len(self.p2_deck) > 0): # using i and j, modular
                     roundwin = "p1"
                     print(self.p1_deck[0] > self.p2_deck[0])
                     print("Player 1 wins this round")
-                    self.p1_deck.append(self.p1_deck[0])
-                    self.p1_deck.append(self.p2_deck[0])
+                    cards_to_stack = [self.p1_deck[0], self.p2_deck[0]]
+                    shuffle(cards_to_stack)
+                    self.p1_deck.extend(cards_to_stack)
                     self.p1_deck.pop(0) # make sure to remove top card from deck
                     self.p2_deck.pop(0)
+                    if self.is_war is True:
+                        cards_to_stack = [self.p1_deck[1], self.p1_deck[2],
+                                          self.p2_deck[1], self.p2_deck[2]]
+                        shuffle(cards_to_stack)
+                        self.p1_deck.extend(cards_to_stack)
+                        self.p1_deck.pop(1)
+                        self.p1_deck.pop(2)
+                        self.p2_deck.pop(1)
+                        self.p2_deck.pop(2)
+                        self.is_war = False
                     print(f"p1 has {len(self.p1_deck)} cards") # debug
                     print(f"p2 has {len(self.p2_deck)} cards") # debug
 
-                elif self.p1_deck[i] < self.p2_deck[j]:
+                elif (self.p1_deck[i] < self.p2_deck[j]) and (len(self.p1_deck) > 0 and len(self.p2_deck) > 0):
                     roundwin = "p2"
                     print(self.p1_deck[0] < self.p2_deck[0])
                     print("Player 2 wins this round")
-                    self.p2_deck.append(self.p1_deck[0])
-                    self.p2_deck.append(self.p2_deck[0])
+                    cards_to_stack = [self.p1_deck[0], self.p2_deck[0]]
+                    shuffle(cards_to_stack)
+                    self.p2_deck.extend(cards_to_stack)
                     self.p1_deck.pop(0) # make sure to remove top card from deck
                     self.p2_deck.pop(0)
+                    if self.is_war is True:
+                        cards_to_stack = [self.p1_deck[1], self.p1_deck[2],
+                                          self.p2_deck[1], self.p2_deck[2]]
+                        shuffle(cards_to_stack)
+                        self.p2_deck.extend(cards_to_stack)
+                        self.p2_deck.pop(1)
+                        self.p2_deck.pop(2)
+                        self.p1_deck.pop(1)
+                        self.p1_deck.pop(2)
+                        self.is_war = False
                     print(f"p1 has {len(self.p1_deck)} cards") # debug
                     print(f"p2 has {len(self.p2_deck)} cards") # debug
 
-                elif (self.p1_deck[i] == self.p2_deck[j]) and (len(self.p1_deck) or len(self.p2_deck) > 3):
+                elif (self.p1_deck[i] == self.p2_deck[j]) and (len(self.p1_deck) > 3 and len(self.p2_deck) > 3):
                     self.index_tracker["i"] = 2 # now i and j are mapped to 2, as we use the 3rd card for both decks here
                     self.index_tracker["j"] = 2 # this is only temporary, i and j are set back to 0 right after
+                    self.is_war = True
                     self.round() # should now check for conditions using i and j as index (recursive)
-                    if roundwin == "p1":
-                        self.p1_deck.append(self.p1_deck[1])
-                        self.p1_deck.append(self.p1_deck[2])
-                        self.p1_deck.pop(1)
-                        self.p1_deck.pop(2)
-                        self.p2_deck.pop(1)
-                        self.p2_deck.pop(2)
-                    elif roundwin == "p2":
-                        self.p2_deck.append(self.p2_deck[1])
-                        self.p2_deck.append(self.p2_deck[2])
-                        self.p2_deck.pop(1)
-                        self.p2_deck.pop(2)
-                        self.p1_deck.pop(1)
-                        self.p1_deck.pop(2)
+
+                else: # should see that player decks are depleted
+                    print("Game is over!")
+                    exit()
+
                 self.index_tracker["i"] = 0 # reset both i and j to 0 for further rounds
                 self.index_tracker["j"] = 0 # should work just fine
                 self.roundcount += 1
@@ -159,6 +189,7 @@ def wargame():
     """
     deck = Game()
     deck.round()
-    
+
 if __name__ == "__main__":
     wargame()
+    # End-of-file (EOF)
